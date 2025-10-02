ba3d13684406aefb53a647d7c9958897517dc50e
Dexter <dexterong94@gmail.com>
2025-10-01 21:52:40 -0400

    Working teleop for moving

--- unitree_teleop/include/unitree_teleop/unitree_teleop.hpp
+++ unitree_teleop/include/unitree_teleop/unitree_teleop.hpp
@@ -5,6 +5,10 @@
 #include <sensor_msgs/msg/joy.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_msgs/msg/bool.hpp>
+#include <thread>
+#include "unitree_api/msg/request.hpp"
+#include "unitree_go/msg/sport_mode_state.hpp"
+#include "unitree_teleop/common/ros2_sport_client.h"
 
 class UnitreeTeleop : public rclcpp::Node
 {
@@ -15,15 +19,21 @@ private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
     void twist_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
     void pub_callback();
+    void high_state_handler(const unitree_go::msg::SportModeState::SharedPtr msg);
+    void get_init_state();
 
     // Publishers and subscribers
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+    rclcpp::Subscription<unitree_go::msg::SportModeState>::SharedPtr highstate_sub_;
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr is_auto_pub_;
     
     // Timer
     rclcpp::TimerBase::SharedPtr pub_timer_;
+
+    // Sport client for robot communication
+    SportClient sport_client_;
+    unitree_api::msg::Request req_;
 
     // State variables
     geometry_msgs::msg::Twist twist_buf_;
